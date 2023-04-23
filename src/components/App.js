5dcadb6774ddf4ecff49fdf561c0dcf42e0c04a7
Rahul Singh <rahulme0083@gmail.com>
2023-04-23 16:12:49 +0530

    update

--- src/components/App.js
+++ src/components/App.js
@@ -10,19 +10,52 @@ function App() {
   const [result, setResult] = useState('');
   const [index, setIndex] = useState(0);
 
+  useEffect(()=>{
+    const timer=setTimeout(()=>{
+
+      setFlashWord(false);
+    },500);
+
+    return()=>clearTimeout(timer);
+  },[flashWord])
+
+  const handleInputChange=(event)=>{
+    setUserInput(event.target.value);
+  }
+
+  const handleFormSubmit=(event)=>{
+
+    event.preventDefault();
+    if(userInput.toLowerCase()===WORD_LIST[index]){
+      setResult("You Won!");
+    }
+    else{
+      setResult("You Lost!");
+    }
+  }
+  const handleRestartClick=()=>{
+     setIndex(index+1);
+     setFlashWord(true);
+     setResult("");
+     setUserInput("");
+    setTimeout(()=>{
+      setFlashWord(false)
+    },500)
+
+  }
 
   return (
-    <div class="mini-game-container">
-      <h2 class="mini-game-title">Mini Game</h2>
-      <p class="mini-game-word">{word}</p>
-      <form class="mini-game-form" onSubmit={handleFormSubmit}>
-        <input class="mini-game-input" type="text" value={userInput} onChange={handleInputChange} />
-        <button class="mini-game-button" type="submit">Check Answer</button>
+    <div className="mini-game-container">
+      <h2 className="mini-game-title">Mini Game</h2>
+    {flashWord &&  <p className="mini-game-word">{WORD_LIST[index]}</p>}
+      <form className="mini-game-form" onSubmit={handleFormSubmit}>
+        <input className="mini-game-input" type="text" value={userInput} onChange={handleInputChange} />
+        <button className="mini-game-button" type="submit">Check Answer</button>
       </form>
       {result && (
         <>
-          <p class="mini-game-result">{result}</p>
-          <button class="mini-game-restart-button" onClick={handleRestartClick}>Restart</button>
+          <p className="mini-game-result">{result}</p>
+          <button className="mini-game-restart-button" onClick={handleRestartClick}>Restart</button>
         </>
       )}
     </div>
